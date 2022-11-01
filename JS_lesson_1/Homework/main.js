fd9e8de38c4cfe31a376f27b75c89d80e6558981
marketing-specv <marketing.specv@gmail.com>
2022-11-01 11:14:12 +0200

    js_1_homework

---
+++ JS_lesson_1/Homework/main.js
@@ -0,0 +1,40 @@
+let v1 = 'hello';
+console.log(v1);
+let v2 = 'owu';
+console.log(v2);
+let v3 = 'com';
+console.log(v3);
+let v4 = 'ua';
+console.log(v4);
+let v5 = 1;
+console.log(v5);
+let v6 = 10;
+console.log(v6);
+let v7 = -999;
+console.log(v7);
+let v8 = 123;
+console.log(v8);
+let v9 = 3.14;
+console.log(v9);
+let v10 = 2.7;
+console.log(v10);
+let v11 = 16;
+console.log(v11);
+let v12 = true;
+console.log(v12);
+let v13 = false;
+console.log(13);
+
+let firstName = 'Volodymyr ';
+let middleName = 'Urijovich';
+let lastName = 'Lazarchuk';
+console.log(firstName + middleName + ' ' + lastName);
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+prompt('my name is '+ firstName + middleName, '...and your`s?')
